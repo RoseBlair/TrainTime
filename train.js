0f03b66a0ccd79768d96965090a6f46a6dce9bda
Carolyn Bernier <carolynbbernier@gmail.com>
2018-11-21 21:01:14 -0500

    algorithm

--- train.js
+++ train.js
@@ -13,30 +13,71 @@ $(document).ready(function(){
     // variable to reference the database.
     var database = firebase.database();
 
+     
+function getNextTrainTime(freqTrain1, firstTrainTime) {
 
-    // var nextTrain = 
-    //  ("#arrivalTrain1").text(moment.diff(moment(nextTrain)));
-    //  ("#minutesTrain1").text(nextTrain);
+    var freqTrain1 = parseInt($("#freqTrain1").text());
+    console.log($("#freqTrain1").text());
+    console.log(freqTrain1);
 
-    var currentdate = new Date();
-    var currentTime = "";
-    currentTime = currentdate.getHours() + ":" + currentdate.getMinutes();
+    var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+
+    var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+
+    console.log(diffTime);
+
+    var trainRemainder = diffTime % freqTrain1;
+
+    console.log(trainRemainder);
+
+    var minutesLeft = freqTrain1 - trainRemainder;
+
+    console.log(minutesLeft);
+
+    var nextTrain = moment().add(minutesLeft, "minutes");
+
+    console.log(moment(nextTrain).format("hh:mm"));
+
+};
 
 
-    setInterval(function() {
-        $("#currentTime").html(currentTime);   
-        }, 10000);
 
+var firstTrainTime = "3:00";
 
-    $("#submission").click(function(currentTime) {
-        event.preventDefault();
-
-        var trainName = $("#trainName").val();
+var metroNorth = {name:"Fiat", model:"500", firstTrainTime: 3:00};
         
-        var destination = $("#destination").val().trim();
-        var firstTrainTime = $("#firstTrainTime").val().trim();
-        var convertedTime = $(moment(firstTrainTime, 'HH:mm'));
-        var frequency = $(parseInt("#frequency"));
+
+    //Show the arrival of the next train and the minutes to the next train in HTML. 
+
+    // ("#arrivalTrain1").text(currentTime.diff(moment(nextTrain)));
+    // ("#minutesTrain1").text(nextTrain);
+        
+
+    // // use moment to find the difference between current time and next train.
+
+    //   function minutesTrain1(nextTrain) {
+    //       var incoming = currentTime.diff(moment(nextTrain));
+    //       $("#arrivalTrain1").html(incoming);
+    //       };
+    
+
+    
+
+
+    // setInterval(function() {
+    //     $("#currentTime").html(currentTime);   
+    //     }, 10000);
+
+
+    // $("#submission").click(function(currentTime) {
+    //     event.preventDefault();
+
+    //     var trainName = $("#trainName").val();
+        
+    //     var destination = $("#destination").val().trim();
+    //     var firstTrainTime = $("#firstTrainTime").val().trim();
+    //     var convertedTime = $(moment(firstTrainTime, 'HH:mm'));
+    //     var frequency = $(parseInt("#frequency"));
         
         
         
@@ -53,7 +94,7 @@ $(document).ready(function(){
         // database.ref().on("value", function() {
 
         // });
-    });
+    // });
 
 
 
